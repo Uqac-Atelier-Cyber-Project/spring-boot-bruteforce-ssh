97e8e03042210f8dcdbcaa6e53c990f825ba0751
Aubry TONNERRE <47123188+kiurow590@users.noreply.github.com>
2025-02-07 03:39:24 +0000

    init project file

---
+++ src/main/java/com/uqac/bruteforce_ssh/BruteforceSshApplication.java
@@ -0,0 +1,13 @@
+package com.uqac.bruteforce_ssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruteforceSshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruteforceSshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uqac/bruteforce_ssh/BruteforceSshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqac.bruteforce_ssh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruteforceSshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
